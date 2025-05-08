4c639baff43e2e96e050b5fcf48bac3a23b282ca
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-05-08 18:17:35 +0200

    chore: improve layout

--- src/main/scala/store/handytools/View.scala
+++ src/main/scala/store/handytools/View.scala
@@ -6,12 +6,12 @@ import tyrian.Empty
 import Extensions.*
 
 object View {
-  def main_section(model: Model): Html[Msg] =
+  def mainSection(model: Model): Html[Msg] =
     div(_class := "flex flex-col py-4 max-sm:min-h-screen sm:h-screen")(
       heading(),
-      model.currentNote |> notes_input,
-      model |> notes_section,
-      all_notes(model.notes, model.recentlyCopied)
+      model.currentNote |> notesInput,
+      model |> notesSection,
+      allNotes(model.notes, model.recentlyCopied)
     )
 
   private def heading(): Html[Msg] =
@@ -21,7 +21,7 @@ object View {
       )
     )
 
-  private def notes_input(note: Option[PotentialNote]): Html[Msg] =
+  private def notesInput(note: Option[PotentialNote]): Html[Msg] =
     val (verb, focusBorderClass, bgClass) = note.flatMap(_.index) match
       case None    => ("add", "focus:outline-[#b8bb26]", "bg-[#b8bb26]")
       case Some(_) => ("update", "focus:outline-[#fe8019]", "bg-[#fe8019]")
@@ -56,23 +56,25 @@ object View {
       )
     )
 
-  private def notes_section(model: Model): Html[Msg] =
+  private def notesSection(model: Model): Html[Msg] =
     if (model.notes.isEmpty) {
-      intro_section()
+      introSection()
     } else {
       val inputs = model.notes.zipWithIndex.map(
-        note_entry(model.currentNote.flatMap(_.index))
+        noteEntry(model.currentNote.flatMap(_.index))
       )
-      val children = List(p(_class := "text-xl mb-2")("Entries")) ++ inputs
 
       div(
-        _class := "md:flex-1 flex flex-col gap-6 mt-4 border-2 border-dotted border-[#928374] border-opacity-10 p-4 max-sm:p-2 md:overflow-y-auto"
+        _class := "mt-4 border-2 border-dotted border-[#928374] border-opacity-10 p-4 max-sm:p-2 md:overflow-y-auto"
       )(
-        children*
+        p(_class := "text-xl")("Entries"),
+        div(_class := "mt-4 md:flex-1 flex flex-col gap-6")(
+          inputs*
+        )
       )
     }
 
-  private def intro_section(): Html[Msg] =
+  private def introSection(): Html[Msg] =
     div(
       _class := "flex flex-col gap-4 mt-4 border-2 border-dotted border-[#928374] border-opacity-10 p-4 max-sm:p-2 overflow-x-auto md:overflow-y-auto"
     )(
@@ -104,14 +106,14 @@ Y88b.    888  888 888    Y88..88P 888  888 Y88..88P Y88b. Y8b.          X88
         "(yes, it's for a very small niche :D)"
       ),
       button(
-        _class := "bg-[#8ec07c] text-[#282828] mx-auto mt-4 px-2 py-1 font-semibold cursor-pointer",
+        _class := "bg-[#8ec07c] text-[#282828] mx-auto mt-4 px-4 py-1 font-semibold cursor-pointer",
         onClick(Msg.UserRequestedSampleNotes)
       )(
         "show me some samples"
       )
     )
 
-  private def note_entry(
+  private def noteEntry(
       currentlyEditedIndex: Option[Int]
   )(
       note: Note,
@@ -175,7 +177,7 @@ Y88b.    888  888 888    Y88..88P 888  888 Y88..88P Y88b. Y8b.          X88
       )
     )
 
-  private def all_notes(
+  private def allNotes(
       notes: Vector[Note],
       recentlyCopied: Boolean
   ): Html[Msg] =
